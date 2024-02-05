a592b55c26451d352c282b3ff09d40b148d640ff
yandah1 <nonku003@gmail.com>
2024-02-05 23:19:02 +0200

    I modified imports

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -4,7 +4,11 @@
 from flask import Flask, jsonify, abort, request
 from models import storage
 from api.v1.views import app_views
+from models.amenity import Amenity
+from models.city import City
 from models.place import Place
+from models.state import State
+from models.user import User
 
 
 @app_views.route('/cities/<city_id>/places', methods=['GET'],
